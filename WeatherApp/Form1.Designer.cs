1ba1c85f1633638469d81f06aace4003588974ae
LSDG0d <grabelnikov757@gmail.com>
2023-12-25 11:48:07 +0300

    Add project files.

---
+++ WeatherApp/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace WeatherApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CityTextBox = new System.Windows.Forms.TextBox();
+            this.TemperatureLabel = new System.Windows.Forms.Label();
+            this.DescriptionLabel = new System.Windows.Forms.Label();
+            this.WindSpeedLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(316, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Подтвердить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите город:";
+            // 
+            // CityTextBox
+            // 
+            this.CityTextBox.Location = new System.Drawing.Point(156, 44);
+            this.CityTextBox.Name = "CityTextBox";
+            this.CityTextBox.Size = new System.Drawing.Size(154, 30);
+            this.CityTextBox.TabIndex = 2;
+            // 
+            // TemperatureLabel
+            // 
+            this.TemperatureLabel.AutoSize = true;
+            this.TemperatureLabel.Location = new System.Drawing.Point(12, 150);
+            this.TemperatureLabel.Name = "TemperatureLabel";
+            this.TemperatureLabel.Size = new System.Drawing.Size(163, 22);
+            this.TemperatureLabel.TabIndex = 3;
+            this.TemperatureLabel.Text = "Температура: N/A";
+            // 
+            // DescriptionLabel
+            // 
+            this.DescriptionLabel.AutoSize = true;
+            this.DescriptionLabel.Location = new System.Drawing.Point(12, 172);
+            this.DescriptionLabel.Name = "DescriptionLabel";
+            this.DescriptionLabel.Size = new System.Drawing.Size(140, 22);
+            this.DescriptionLabel.TabIndex = 4;
+            this.DescriptionLabel.Text = "Описание: N/A";
+            // 
+            // WindSpeedLabel
+            // 
+            this.WindSpeedLabel.AutoSize = true;
+            this.WindSpeedLabel.Location = new System.Drawing.Point(12, 194);
+            this.WindSpeedLabel.Name = "WindSpeedLabel";
+            this.WindSpeedLabel.Size = new System.Drawing.Size(185, 22);
+            this.WindSpeedLabel.TabIndex = 5;
+            this.WindSpeedLabel.Text = "Скорость ветра: N/A";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 22);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Результат:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 548);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.WindSpeedLabel);
+            this.Controls.Add(this.DescriptionLabel);
+            this.Controls.Add(this.TemperatureLabel);
+            this.Controls.Add(this.CityTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MainForm";
+            this.Text = "WeatherApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox CityTextBox;
+        private System.Windows.Forms.Label TemperatureLabel;
+        private System.Windows.Forms.Label DescriptionLabel;
+        private System.Windows.Forms.Label WindSpeedLabel;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WeatherApp/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherApp
+{
+    public partial class MainForm : Form
+    {
+        // Ключ API для OpenWeatherMap
+        private const string ApiKey = "ff88341c4cf15808a85ef21762d0845f";
+
+        // URL для получения данных о текущей погоде
+        private const string ApiUrl = "https://api.openweathermap.org/data/2.5/weather";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Получаем название города из текстового поля
+            string city = CityTextBox.Text;
+            if (string.IsNullOrEmpty(city))
+            {
+                MessageBox.Show("Введите название города");
+                return;
+            }
+
+            // Формируем URL для запроса к OpenWeatherMap API
+            string apiUrl = $"{ApiUrl}?q={city}&units=metric&appid={ApiKey}";
+
+            try
+            {
+                // Отправляем запрос и получаем ответ
+                string response = MakeApiRequest(apiUrl);
+
+                // Парсим ответ для получения данных о погоде
+                WeatherData weatherData = ParseWeatherData(response);
+
+                // Выводим информацию о погоде в Label
+                TemperatureLabel.Text = "Температура: " + weatherData.Temperature.ToString();
+                DescriptionLabel.Text = "Описание: " + weatherData.Description.ToString();
+                WindSpeedLabel.Text = "Скорость ветра: " + weatherData.WindSpeed.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
+
+        // Метод для отправки HTTP-запроса и получения ответа в виде строки
+        private string MakeApiRequest(string apiUrl)
+        {
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(apiUrl);
+            using (HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse())
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                return streamReader.ReadToEnd();
+            }
+        }
+
+        // Метод для парсинга JSON-данных о погоде
+        private WeatherData ParseWeatherData(string json)
+        {
+            var data = JObject.Parse(json);
+            double temperature = data["main"]["temp"].Value<double>();
+            string description = data["weather"][0]["description"].Value<string>();
+            double windSpeed = data["wind"]["speed"].Value<double>();
+
+            return new WeatherData
+            {
+                Temperature = temperature,
+                Description = description,
+                WindSpeed = windSpeed
+            };
+        }
+    }
+
+    // Класс для хранения данных о погоде
+    public class WeatherData
+    {
+        public double Temperature { get; set; }
+        public string Description { get; set; }
+        public double WindSpeed { get; set; }
+    }
+}
